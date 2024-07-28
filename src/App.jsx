c5c117418ab11ac5f6c17fc6e9eaf6e6413a2aa2
NiclasNN <niclasnorman1@hotmail.se>
2024-07-28 10:35:51 +0200

    fixed bakground with rolling cubes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-// App.jsx
 import React from 'react';
 import Navbar from './Navbars/navbar';
 import Container from './Containers/Container';
 import './App.css';
 
-
-
 function App() {
     return (
-      <div className="App">
-        <Navbar />
-        <Container>
-          
-          {/* Annat innehåll kan läggas till här */}
-        </Container>
-      </div>
+        <div className="App">
+            <div className="area">
+                <ul className="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <Navbar />
+            <Container>
+                <div className="context">
+                    
+                </div>
+                {/* Annat innehåll kan läggas till här */}
+            </Container>
+        </div>
     );
-  }
-  
-  export default App;
+}
+
+export default App;
--- src/Containers/Container.jsx
+++ src/Containers/Container.jsx
@@ -1,35 +1,29 @@
 import React from 'react';
 import './container.css';
-import Comet from './comet';
 import About from '../aboutme/About';
 import Projects from '../myprojects/Projects';
-import Codelanguage from '../codelanguages/Codelanguage'; 
-<title>Niclas Norman portfolio
-  
-</title>
+import Codelanguage from '../codelanguages/Codelanguage';
+
 const Container = ({ children }) => {
   return (
-    
     <div className="container">
-      <Comet />
       <div className="boxes">
-        <div id="boxen2"> 
+        <div id="boxen2">
           <h2>Niclas Norman</h2>
           <h1>Frontend developer</h1>
         </div>
         <div id="boxen">
           <img src="/jag.png" alt="Bild på mig" className="my-image" />
           {children}
-          
         </div>
-        
       </div>
       <About />
-      <Codelanguage /> 
-      <Projects/>
-      <div id ="contactme"><h2>kontakta mig</h2></div>
+      <Codelanguage />
+      <Projects />
+      <div id="contactme">
+        <h2>kontakta mig</h2>
+      </div>
     </div>
-    
   );
 };
 
